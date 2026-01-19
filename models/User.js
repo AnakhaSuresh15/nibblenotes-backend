041f35bdcaf0b33d6d99b7da91c1b1f6cd6a554e
Anakha Suresh <anakhasuresh15@gmail.com>
2026-01-19 20:29:57 +0530

    api error fix

--- models/User.js
+++ models/User.js
@@ -2,11 +2,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
   firstName: { type: String, trim: true, default: "" },
   lastName: { type: String, trim: true, default: "" },
   email: {
